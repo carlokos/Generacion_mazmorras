891aecfc758acff74f06bd6c534929717571e5c1
carlokos <carlokos7@gmail.com>
2023-04-17 12:35:23 +0200

    terminado

---
+++ RogueLike/Assets/Scripts/ComportamientoSala.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamientoSala : MonoBehaviour
+{
+
+    public GameObject[] muro; // 0- top, 1- down, 2- left, 3- right
+    public GameObject[] puerta;
+
+
+    // Update is called once per frame
+    public void UpdateSala(bool[] status)
+    {
+        for(int i = 0; i < status.Length; i++)
+        {
+            puerta[i].SetActive(status[i]);
+            muro[i].SetActive(!status[i]);
+        }       
+    }
+}
